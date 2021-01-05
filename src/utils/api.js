35a781671a26384e98d8be772d127166a7094793
Ramon <ramondavi@msn.com>
2021-01-04 21:52:29 -0300

    trying to fix bug

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": "http://localhost:3000/"
+    "Access-Control-Allow-Origin": "https://teste-codeby.herokuapp.com/"
   },
   withCredentials: true
 });
