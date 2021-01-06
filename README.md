f1ae0409400a763f453cc070a156c967a987d130
Ramon <ramondavi@msn.com>
2021-01-06 17:08:19 -0300

    fetch url changed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # teste-codeby
 
 Processo Seletivo da Codeby para desenvolvedor front end pleno
+
+## execubar
+
+Vá ao diretório e dê um npm install, após esperar a instalação dos packages use o comando npm start e abra o navegador no porto 3000
--- src/redux/item/item.actions.js
+++ src/redux/item/item.actions.js
@@ -8,7 +8,7 @@ import {
 export const getItems = () => async dispatch => {
   try {
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/11b895d0-bc64-4f3a-bfa9-7c652be8d415/acima-10-reais.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210104%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210104T154759Z&X-Amz-Expires=86400&X-Amz-Signature=4ebc287f36c19d66148038c27f5b425564c5e574de6f898a55242118c459e45f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22acima-10-reais.json%22'
+    targetUrl = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/11b895d0-bc64-4f3a-bfa9-7c652be8d415/acima-10-reais.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210106%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210106T200620Z&X-Amz-Expires=86400&X-Amz-Signature=cb3c7d777da241bbd6e6be923c9d2cf878f3d8e5c7c13c8b142aea52f4a0ecf6&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22acima-10-reais.json%22'
     fetch(proxyUrl + targetUrl)
     .then(blob => blob.json())
     .then(data => {
